3514124be352305f6a3785f58206f59685026462
imanmachukwu <iamimanma@gmail.com>
2025-04-16 04:44:17 +0100

    Initial Commit

---
+++ Weekly Project Tracker/Weekly_Project_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_Project_TrackerApp.swift
+//  Weekly Project Tracker
+//
+//  Created by Imanma's MacBook  on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_Project_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
